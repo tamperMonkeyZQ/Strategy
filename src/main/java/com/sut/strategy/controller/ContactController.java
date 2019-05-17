e3d61588d770ffbcd30172fefb4367ae1c4e4284
zhangq <includeiostreamzq@gmail.com>
2019-05-17 11:17:08 +0800

    新建dev分支

--- src/main/java/com/sut/strategy/controller/ContactController.java
+++ src/main/java/com/sut/strategy/controller/ContactController.java
@@ -17,6 +17,7 @@ public class ContactController {
     public ModelAndView getContactList(@RequestParam int type) {
         ModelAndView modelAndView = new ModelAndView("contact/list");
         modelAndView.addObject("contactList", tsUserService.findAllByType(type, 1, 10));
+        System.out.println("");
         return modelAndView;
     }
 }
